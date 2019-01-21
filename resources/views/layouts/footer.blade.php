7babb10cd6bf99a6cc6455895a01a36b0f6b28ae
DESKTOP-ENHS70G\Admin <tt.hau94@gmail.com>
2019-01-22 00:54:27 +0700

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -61,3 +61,12 @@
         </div>
     </div>
 </div>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132872367-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-132872367-1');
+</script>
