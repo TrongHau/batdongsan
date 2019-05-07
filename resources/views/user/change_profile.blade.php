8ae78b905258214e93c36be3681686ca5bc3fa95
DESKTOP-ENHS70G\Admin <tt.hau94@gmail.com>
2019-05-08 01:07:38 +0700

    no message

--- resources/views/user/change_profile.blade.php
+++ resources/views/user/change_profile.blade.php
@@ -296,7 +296,7 @@ $mySelf = Auth::user();
                                     <td></td>
                                     <td>
                                         <img src="/captcha/image.php" id="img-captcha" style="float: left;border: 1px solid #e8e8e8;"/>
-                                        <img type="button" src="/imgs/icon-reload.png" value="Reload" onclick="$('#img-captcha').attr('src', '/captcha/image.php?rand=' + Math.random())"  style="float: left; margin-top: 4px; margin-left: 4px;"/>
+                                        <img type="button" src="/imgs/icon-reload.png" value="Reload" onclick="$('#img-captcha').attr('src', '/captcha/image.php?rand=' + Math.random())"  style="float: left; margin-top: 7px; margin-left: 5px;"/>
                                     </td>
                                 </tr>
                                 <tr>
