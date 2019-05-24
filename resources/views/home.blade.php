f397d061a194a1cdfc10385c9f84489d213a54b1
CSN\admin <tt.hau94@gmail.com>
2019-05-24 14:12:14 +0700

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -146,12 +146,10 @@ $Agent = new Agent();
                                             <div class="p-bottom-crop">
                                                 <div class="p-bottom-left">
                                                     <div>
-                                                        <div class="left">Giá</div>
-                                                        :&nbsp;{{($item->price_from != null && $item->price_to != null) ? ($item->price_from. ' - ' .$item->price_to.' '.$item->ddlPriceType) : ($item->price_real == 0 ? 'Thỏa thuận' : $item->price.' '.$item->ddlPriceType)}}
+                                                        <div class="left">Giá</div><div class="right_m">:&nbsp;{{($item->price_from != null && $item->price_to != null) ? ($item->price_from. ' - ' .$item->price_to.' '.$item->ddlPriceType) : ($item->price_real == 0 ? 'Thỏa thuận' : $item->price.' '.$item->ddlPriceType)}}</div>
                                                     </div>
                                                     <div>
-                                                        <div class="left">Diện tích</div>
-                                                        :&nbsp;{{($item->area_from != null && $item->area_to != null) ? ($item->area_from. ' - ' .$item->area_to.' m²') : ($item->area ? $item->area.' m²' : 'Chưa xác định')}}
+                                                        <div class="left">Diện tích</div><div class="right_m">:&nbsp;{{($item->area_from != null && $item->area_to != null) ? ($item->area_from. ' - ' .$item->area_to.' m²') : ($item->area ? $item->area.' m²' : 'Chưa xác định')}}</div>
                                                     </div>
                                                     <div>
                                                         <?php
