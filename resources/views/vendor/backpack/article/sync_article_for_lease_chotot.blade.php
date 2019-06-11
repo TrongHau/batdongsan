1a7f2bd703f0b47e52294ecb8aa5ad9592b8e653
CSN\admin <tt.hau94@gmail.com>
2019-06-12 02:12:49 +0700

    no message

---
+++ resources/views/vendor/backpack/article/sync_article_for_lease_chotot.blade.php
@@ -0,0 +1,74 @@
+@extends('backpack::layout')
+
+@section('header')
+    <section class="content-header">
+        <h1>
+            <span class="text-capitalize">Lấy Tin Rao Cho Thuê</span>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="http://batdongsan.localhost/admin/dashboard">Admin</a></li>
+            <li><a href="http://batdongsan.localhost/admin/sync_article" class="text-capitalize">Lấy Tin Rao Bán - Cho Thuê Chotot</a></li>
+        </ol>
+    </section>
+@endsection
+
+@section('content')
+    <section class="content">
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <!-- Default box -->
+                <a href="/admin/sync_chotot_article_for_lease" class="hidden-print"><i class="fa fa-angle-double-left"></i> Back to all  <span>Lấy Tin Rao Cho Thuê Chotot</span></a><br><br>
+
+
+                <form method="post" action="/admin/sync_chotot_article_for_lease/sync_post_article">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="box">
+
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Bắt đầu lấy tin tức</h3>
+                        </div>
+                        <div class="box-body row display-flex-wrap" style="display: flex; flex-wrap: wrap;">
+                            <!-- load the view from the application if it exists, otherwise load the one in the package -->
+                            <!-- load the view from type and view_namespace attribute if set -->
+
+                            <!-- text input -->
+                            <div class="form-group col-xs-4">
+                                <label>Thể loại</label>
+                                <select name="method_article" class="form-control">
+                                    <option value="Nhà đất bán">Nhà đất bán</option>
+                                    <option value="Nhà đất cho thuê">Nhà đất cho thuê</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-xs-4">
+                                <label>Danh Mục</label>
+                                <select name="type_article" class="form-control">
+                                    <option value="Bán căn hộ chung cư">Căn hộ/Chung cư</option>
+                                    <option value="Bán nhà riêng">Nhà ở</option>
+                                    <option value="Bán đất">Đất</option>
+                                    <option value="Bán loại bất động sản khác">Văn phòng, Mặt bằng kinh doanh</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-xs-12">
+                                <label>Nhập mã code tin rao mua chotot</label>
+                                <textarea name="str_html" class="form-control" rows="15"></textarea>
+                            </div>
+                        </div>
+                        <div class="box-footer">
+
+                            <div id="saveActions" class="form-group">
+
+                                <button type="submit" class="btn btn-success" value="" ><span class="fa fa-cloud-download"></span> &nbsp;Bắt đầu lấy tin</button>
+                                <a href="/admin/sync_article_for_lease_chotot" class="btn btn-default"><span class="fa fa-ban"></span> &nbsp;Cancel</a>
+                            </div>
+
+                        </div><!-- /.box-footer-->
+
+                    </div><!-- /.box -->
+                </form>
+            </div>
+        </div>
+
+
+    </section>
+@endsection
---
+++ resources/views/vendor/backpack/crud/edit_sync_chotot_for_buy_article.blade.php
@@ -0,0 +1,97 @@
+@extends('backpack::layout')
+
+@section('header')
+    <section class="content-header">
+        <h1>
+            <span class="text-capitalize">{{ $crud->entity_name_plural }}</span>
+            <small>{{ trans('backpack::crud.edit').' '.$crud->entity_name }}.</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="{{ url(config('backpack.base.route_prefix'),'dashboard') }}">{{ trans('backpack::crud.admin') }}</a></li>
+            <li><a href="{{ url($crud->route) }}" class="text-capitalize">{{ $crud->entity_name_plural }}</a></li>
+            <li class="active">{{ trans('backpack::crud.edit') }}</li>
+        </ol>
+    </section>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <!-- Default box -->
+            @if ($crud->hasAccess('list'))
+                <a href="{{ url($crud->route) }}" class="hidden-print"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a><br><br>
+            @endif
+
+            @include('crud::inc.grouped_errors')
+
+            <form method="post"
+                  action="{{ url($crud->route.'/'.$entry->getKey()) }}"
+                  @if ($crud->hasUploadFields('update', $entry->getKey()))
+                  enctype="multipart/form-data"
+                    @endif
+            >
+                {!! csrf_field() !!}
+                {!! method_field('PUT') !!}
+                <div class="box">
+                    <div class="box-header with-border">
+                    @if ($crud->model->translationEnabled())
+                        <!-- Single button -->
+                            <div class="btn-group pull-right">
+                                <button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{trans('backpack::crud.language')}}: {{ $crud->model->getAvailableLocales()[$crud->request->input('locale')?$crud->request->input('locale'):App::getLocale()] }} <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    @foreach ($crud->model->getAvailableLocales() as $key => $locale)
+                                        <li><a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}?locale={{ $key }}">{{ $locale }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <h3 class="box-title" style="line-height: 30px;">{{ trans('backpack::crud.edit') }}</h3>
+                        @else
+                            <h3 class="box-title">{{ trans('backpack::crud.edit') }}</h3>
+                        @endif
+                    </div>
+                    <div class="box-body row display-flex-wrap" style="display: flex;flex-wrap: wrap;">
+                        <!-- load the view from the application if it exists, otherwise load the one in the package -->
+                        @if(view()->exists('vendor.backpack.crud.form_content'))
+                            @include('vendor.backpack.crud.form_content', ['fields' => $fields, 'action' => 'edit'])
+                        @else
+                            @include('crud::form_content', ['fields' => $fields, 'action' => 'edit'])
+                        @endif
+                    </div><!-- /.box-body -->
+
+                    <div class="box-footer">
+                        <div id="saveActions" class="form-group">
+
+                            <input type="hidden" name="save_action" value="{{ $saveAction['active']['value'] }}">
+                            <a href="/admin/sync_chotot_article_for_buy/approval/{{$fields['id']['value']}}" class="btn btn-warning" style="background-color: #000000; border-color: #171615;"><span class="fa fa-check-square-o"></span> &nbsp;Duyệt tin này</a>
+                            <div class="btn-group">
+
+                                <button type="submit" class="btn btn-success">
+                                    <span class="fa fa-save" role="presentation" aria-hidden="true"></span> &nbsp;
+                                    <span data-value="{{ $saveAction['active']['value'] }}">{{ $saveAction['active']['label'] }}</span>
+                                </button>
+
+                                <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aira-expanded="false">
+                                    <span class="caret"></span>
+                                    <span class="sr-only">&#x25BC;</span>
+                                </button>
+
+                                <ul class="dropdown-menu">
+                                    @foreach( $saveAction['options'] as $value => $label)
+                                        <li><a href="javascript:void(0);" data-value="{{ $value }}">{{ $label }}</a></li>
+                                    @endforeach
+                                </ul>
+
+                            </div>
+
+                            <a href="{{ $crud->hasAccess('list') ? url($crud->route) : url()->previous() }}" class="btn btn-default"><span class="fa fa-ban"></span> &nbsp;{{ trans('backpack::crud.cancel') }}</a>
+                        </div>
+
+
+                    </div><!-- /.box-footer-->
+                </div><!-- /.box -->
+            </form>
+        </div>
+    </div>
+@endsection
