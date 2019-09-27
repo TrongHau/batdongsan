96bbfff0cdb590a6529d4ef80825e493e364ec4c
CSN\admin <tt.hau94@gmail.com>
2019-09-27 11:30:42 +0700

    change google analytics

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -67,12 +67,11 @@
         </div>
     </div>
 </div>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132872367-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132872367-2"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-132872367-1');
+    gtag('config', 'UA-132872367-2');
 </script>
