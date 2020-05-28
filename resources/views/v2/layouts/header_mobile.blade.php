ea6c18972ee3af4647eea14ef3c18b59bf5e0907
CSN\admin <tt.hau94@gmail.com>
2020-05-28 12:53:14 +0700

    no message

---
+++ resources/views/v2/layouts/header_mobile.blade.php
@@ -0,0 +1,158 @@
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="/imgs/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="audience" content="general">
+    <meta name="resource-type" content="document">
+    <meta name="abstract" content="Thông tin nhà đất Việt Nam">
+    <meta name="area" content="Nhà đất và bất động sản">
+    <meta name="placename" content="Việt Nam">
+    <title>
+        Kênh thông tin bất động sản - Mua bán nhà đất số 1 Việt Nam
+    </title>
+    @yield('meta')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="pingback" href="xmlrpc.php">
+    <title>Homesweet &#8211; Just another WordPress site</title>
+    <meta property="og:title" content="Home 1"/>
+    <link rel='dns-prefetch' href='http://maps.google.com/'/>
+    <link rel='dns-prefetch' href='http://api-idx.diversesolutions.com/'/>
+    <link rel='dns-prefetch' href='http://maps.googleapis.com/'/>
+    <link rel='dns-prefetch' href='http://fonts.googleapis.com/'/>
+    <link rel='dns-prefetch' href='http://s.w.org/'/>
+    <link rel="alternate" type="application/rss+xml" title="Homesweet &raquo; Feed" href="feed/index.html"/>
+    <link rel="alternate" type="application/rss+xml" title="Homesweet &raquo; Comments Feed"
+          href="comments/feed/index.html"/>
+    <link rel='stylesheet' id='dsidx-css' href='https://api-idx.diversesolutions.com/combo-css?config=dsidxpress'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='dsidxpress-icons-css'
+          href='/wp-content/plugins/dsidxpress/css/dsidx-iconsbf11.css?ver=4.8.13' type='text/css' media='all'/>
+    <link rel='stylesheet' id='dsidxpress-unconditional-css'
+          href='/wp-content/plugins/dsidxpress/css/clientbf11.css?ver=4.8.13' type='text/css' media='all'/>
+    <link rel='stylesheet' id='dsidxwidgets-unconditional-css'
+          href='/wp-content/plugins/dsidxpress/dsidxwidgets/css/clientbf11.css?ver=4.8.13' type='text/css' media='all'/>
+    <link rel='stylesheet' id='mapescape.css-css'
+          href='/wp-content/plugins/realia/libraries/mapescape/css/mapescape8f31.css?ver=20160604' type='text/css'
+          media='all'/>
+    <link rel='stylesheet' id='contact-form-7-css'
+          href='/wp-content/plugins/contact-form-7/includes/css/styles33a6.css?ver=4.9' type='text/css' media='all'/>
+    <link rel='stylesheet' id='nextend_fb_connect_stylesheet-css'
+          href='/wp-content/plugins/nextend-facebook-connect/buttons/facebook-btnbf11.css?ver=4.8.13' type='text/css'
+          media='all'/>
+    <link rel='stylesheet' id='nextend_twitter_connect_stylesheet-css'
+          href='/wp-content/plugins/nextend-twitter-connect/buttons/twitter-btnbf11.css?ver=4.8.13' type='text/css'
+          media='all'/>
+    <link rel='stylesheet' id='rs-plugin-settings-css'
+          href='/wp-content/plugins/revslider/public/assets/css/settings4ee1.css?ver=5.3.1.5' type='text/css'
+          media='all'/>
+    <style id='rs-plugin-settings-inline-css' type='text/css'>
+        #rs-demo-id {
+        }
+    </style>
+    <link rel='stylesheet' id='homesweet-theme-fonts-css'
+          href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Dosis:300,400,500,600,700,800&amp;subset=latin%2Clatin-ext'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='js_composer_front-css'
+          href='/wp-content/uploads/js_composer/js_composer_front_custom3c21.css?ver=5.1.1' type='text/css' media='all'/>
+    <link rel='stylesheet' id='awesome-css' href='/wp-content/themes/homesweet/css/awesome1849.css?ver=4.7.0'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='font-ionicons-css' href='/wp-content/themes/homesweet/css/ioniconsf427.css?ver=v2.0.0'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='apus-font-css' href='/wp-content/themes/homesweet/css/apus-font8a54.css?ver=1.0.0'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='material-design-iconic-font-css'
+          href='/wp-content/themes/homesweet/css/material-design-iconic-font3601.css?ver=2.2.0' type='text/css'
+          media='all'/>
+    <link rel='stylesheet' id='animate-css' href='/wp-content/themes/homesweet/css/animate3b71.css?ver=3.5.0'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='bootstrap-css' href='/wp-content/themes/homesweet/css/bootstrap55a0.css?ver=3.2.0'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='homesweet-template-css' href='/wp-content/themes/homesweet/css/templatecf1b.css?ver=3.2'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='homesweet-style-css' href='/wp-content/themes/homesweet/stylecf1b.css?ver=3.2'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='magnific-popup-css'
+          href='/wp-content/themes/homesweet/js/magnific/magnific-popupf488.css?ver=1.1.0' type='text/css' media='all'/>
+    <link rel='stylesheet' id='colorbox-css' href='/wp-content/themes/homesweet/js/colorbox/colorboxf488.css?ver=1.1.0'
+          type='text/css' media='all'/>
+    <link rel='stylesheet' id='perfect-scrollbar-css'
+          href='/wp-content/themes/homesweet/css/perfect-scrollbard0c7.css?ver=0.6.12' type='text/css' media='all'/>
+    <script type='text/javascript' src='/wp-includes/js/jquery/jqueryb8ff.js?ver=1.12.4'></script>
+    <script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1'></script>
+    <script type='text/javascript'>
+        /* <![CDATA[ */
+        var dsidxAjaxHandler = {"ajaxurl": "https:\/\/demoapus.com\/homesweet\/wp-admin\/admin-ajax.php"};
+        /* ]]> */
+    </script>
+    <script type='text/javascript'
+            src='https://api-idx.diversesolutions.com/combo-js?config=dsidxpress&amp;ver=2.4.1'></script>
+    <script type='text/javascript'
+            src='/wp-content/plugins/realia/libraries/jquery.chained.remote.custom.minbf11.js?ver=4.8.13'></script>
+    <script type='text/javascript'
+            src='/wp-content/plugins/revslider/public/assets/js/jquery.themepunch.tools.min4ee1.js?ver=5.3.1.5'></script>
+    <script type='text/javascript'
+            src='/wp-content/plugins/revslider/public/assets/js/jquery.themepunch.revolution.min4ee1.js?ver=5.3.1.5'></script>
+    <script type='text/javascript'
+            src='http://maps.googleapis.com/maps/api/js?libraries=places&amp;language=en_US&amp;key=AIzaSyAcJXszijF-rzxAWR2-afFzjKDyHAF5YTU&amp;ver=1.0'></script>
+    <script type='text/javascript'
+            src='/wp-content/themes/homesweet/js/maps/jquery-google-map-custom62ea.js?ver=1.2'></script>
+    <script type='text/javascript' src='/wp-content/themes/homesweet/js/chart.min5152.js?ver=1.0'></script>
+    <link rel='https://api.w.org/' href='wp-json/index.html'/>
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.php?rsd"/>
+    <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="/wp-includes/wlwmanifest.xml"/>
+    <meta name="generator" content="WordPress 4.8.13"/>
+    <link rel="canonical" href="index.html"/>
+    <link rel='shortlink' href='index.html'/>
+    <link rel="alternate" type="application/json+oembed"
+          href="wp-json/oembed/1.0/embed8db6.json?url=https%3A%2F%2Fdemoapus.com%2Fhomesweet%2F"/>
+    <link rel="alternate" type="text/xml+oembed"
+          href="wp-json/oembed/1.0/embedc035.json?url=https%3A%2F%2Fdemoapus.com%2Fhomesweet%2F&amp;format=xml"/>
+    <link rel='https://github.com/WP-API/WP-API' href='wp-json.json'/>
+    <meta name="generator" content="Powered by Visual Composer - drag and drop page builder for WordPress."/>
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" type="text/css"
+          href="https://demoapus.com/homesweet/wp-content/plugins/js_composer/assets/css/vc_lte_ie9.min.css"
+          media="screen"><![endif]-->
+    <meta name="generator"
+          content="Powered by Slider Revolution 5.3.1.5 - responsive, Mobile-Friendly Slider Plugin for WordPress with comfortable drag and drop interface."/>
+    <noscript>
+        <style type="text/css"> .wpb_animate_when_almost_visible {
+                opacity: 1;
+            }</style>
+    </noscript>
+    <script src="/js/jquery-1.11.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="/css/custom_v2.css">
+    <style>
+        .vc_custom_1502157337810 {
+            padding-top: 20px !important;
+            padding-bottom: 20px !important;
+            background-color: #114a82!important;
+        }
+    </style>
+    <script>
+        var csrfToken = "{{csrf_token()}}";
+        var loaded = false;
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': csrfToken
+            }
+        });
+        $( document ).ajaxStop(function() {
+            loaded = false;
+        });
+    </script>
+    @yield('contentCSS')
+    <?php
+    use Jenssegers\Agent\Agent;
+    $Agent = new Agent();
+
+    ?>
+    @if($Agent->isMobile())
+        <link rel="stylesheet" type="text/css" href="/css/mobile_v2.css">
+    @endif
+</head>
